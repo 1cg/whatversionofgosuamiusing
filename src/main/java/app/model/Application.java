4a96ce1c9abc59784461ab274926ee56744507fa
hkalidhindi <jrharika@gmail.com>
2017-07-24 13:55:49 -0700

    getting the version numbers

--- src/main/java/app/model/Application.java
+++ src/main/java/app/model/Application.java
@@ -1,9 +1,22 @@
 package app.model;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.logging.Filter;
 
 public class Application {
+
+    private.
+    class FilterVersion implements FilenameFilter{
+        @Override
+        public boolean accept(File dir, String name) {
+            return !name.matches("\\..*");
+        }
+    }
 
     private String name;
     private String fileSystemName;
@@ -32,6 +45,14 @@ public class Application {
         return APPLICATIONS;
     }
 
+    public List<String> getVersions() {
+        File buildsDir = Files.getBuildsDir();
+        String appPathName = buildsDir.getAbsolutePath() +  File.separatorChar + fileSystemName;
+        File appFile = new File(appPathName);
+        assert(appFile.exists());
+        return Arrays.asList(appFile.list(new FilterVersion()));
+    }
+
     @Override
     public String toString() {
         return "Application{" +
@@ -39,4 +60,11 @@ public class Application {
                 ", fileSystemName='" + fileSystemName + '\'' +
                 '}';
     }
+
+    public static void main(String[] args) {
+        for (Application app : APPLICATIONS) {
+            System.out.println(app.getVersions().toString());
+        }
+    }
+
 }
