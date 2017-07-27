de777e6c9af1b5d595ab17758522dfc5a2437e61
hkalidhindi <jrharika@gmail.com>
2017-07-26 18:22:00 -0700

    clean up, fixed bug in naming new file in UnzippedFiles directory

--- src/main/java/app/UnzipUtility.java
+++ src/main/java/app/UnzipUtility.java
@@ -31,17 +31,17 @@ public class UnzipUtility {
     }
 
     public static List<File> getFileListFromZip(String zipFilePath) throws IOException {
-        ArrayList<File> filePaths = new ArrayList<>();
+        ArrayList<File> filesList = new ArrayList<>();
         ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
         ZipEntry entry = zipIn.getNextEntry();
         // iterates over entries in the zip file
         while (entry != null) {
-            filePaths.add(new File(entry.getName()));
+            filesList.add(new File(entry.getName()));
             zipIn.closeEntry();
             entry = zipIn.getNextEntry();
         }
         zipIn.close();
-        return filePaths;
+        return filesList;
     }
 
 
@@ -58,7 +58,7 @@ public class UnzipUtility {
         while (entry != null) {
             String entrySimpleName = new File(entry.getName()).getName();
             if (entrySimpleName.equals(fileName)) {
-                filePath = UNZIP_DESTINATION + File.separator + entry.getName();
+                filePath = UNZIP_DESTINATION + File.separator + entrySimpleName;
                 break;
             }
             zipIn.closeEntry();
@@ -70,29 +70,7 @@ public class UnzipUtility {
         return new File(filePath);
     }
 
-    public static void unzip(String zipFilePath, String destDirectory) throws IOException {
-        File destDir = new File(destDirectory);
-        if (!destDir.exists()) {
-            destDir.mkdir();
-        }
-        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
-        ZipEntry entry = zipIn.getNextEntry();
-        // iterates over entries in the zip file
-        while (entry != null) {
-            String filePath = destDirectory + File.separator + entry.getName();
-            if (!entry.isDirectory()) {
-                // if the entry is a file, extracts it
-                extractFile(zipIn, filePath);
-            } else {
-                // if the entry is a directory, make the directory
-                File dir = new File(filePath);
-                dir.mkdir();
-            }
-            zipIn.closeEntry();
-            entry = zipIn.getNextEntry();
-        }
-        zipIn.close();
-    }
+
     private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
         BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
         byte[] bytesIn = new byte[BUFFER_SIZE];
