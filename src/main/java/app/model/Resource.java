74d09f3e991d574f40d035e9961b40d963f805fa
hkalidhindi <jrharika@gmail.com>
2017-07-27 10:54:33 -0700

    throw IOExceptions instead of trying to catch

--- src/main/java/app/model/Resource.java
+++ src/main/java/app/model/Resource.java
@@ -1,6 +1,7 @@
 package app.model;
 
 import app.UnzipUtility;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -42,7 +43,7 @@ public class Resource {
     }
 
     //if is a directory
-    public List<Resource> getResources() {
+    public List<Resource> getResources() throws IOException {
         assert (self.isDirectory() || self.getName().endsWith(".zip") || self.getName().endsWith(".jar"));
         if (resources == null) {
             findResources();
@@ -51,31 +52,21 @@ public class Resource {
     }
 
     //if is a non-directory
-    public String getContent() {
+    public String getContent() throws IOException {
         ensureExtracted();
         assert(self.isFile());
         assert(self.canRead());
-        try {
-            return new String(Files.readAllBytes(Paths.get(self.getAbsolutePath())));
-        } catch (IOException e){
-            //@TODO: handle
-            return null;
-        }
+        return new String(Files.readAllBytes(Paths.get(self.getAbsolutePath())));
     }
 
-    private void findResources() {
+    private void findResources() throws IOException {
         ensureExtracted();
         resources = new ArrayList<>();
 
 
         List<File> releasesFiles;
         if (self.getName().endsWith(".zip") || self.getName().endsWith(".jar")) {
-            try {
-                releasesFiles = UnzipUtility.getFileListFromZip(self.getAbsolutePath());
-            } catch (IOException e) {
-                //TODO: handle error
-                releasesFiles = new ArrayList<>();
-            }
+            releasesFiles = UnzipUtility.getFileListFromZip(self.getAbsolutePath());
         } else {
             assert(self.isDirectory());
             releasesFiles = Arrays.asList(self.listFiles(new FilterFilesStartWNumber()));
@@ -88,15 +79,11 @@ public class Resource {
     }
 
     //if your parent is a zip, extract yourself
-    private void ensureExtracted() {
+    private void ensureExtracted() throws IOException {
         if (!alreadyExtracted) {
             assert(self.exists());
             if (parent.self.getName().endsWith(".zip") || parent.self.getName().endsWith(".jar")) {
-                try {
-                    self = UnzipUtility.unzipFileFromZip(parent.self.getAbsolutePath(), self.getName());
-                } catch (IOException e) {
-                    //TODO: handle error
-                }
+                self = UnzipUtility.unzipFileFromZip(parent.self.getAbsolutePath(), self.getName());
             }
             assert(self.exists());
         }
