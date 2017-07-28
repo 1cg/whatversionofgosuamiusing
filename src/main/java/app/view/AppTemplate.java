35819298eddf113876e28f91db14efa4259b56e1
eddyim <edwardim@berkeley.edu>
2017-07-28 10:59:49 -0700

    Created Helpers to clean up Explore template

--- src/main/java/app/view/AppTemplate.java
+++ src/main/java/app/view/AppTemplate.java
@@ -1,6 +1,9 @@
 package app.view;
 
 import app.model.Application;
+import app.model.Release;
+import app.model.Resource;
+import app.model.Version;
 import bb.runtime.BaseBBTemplate;
 
 import java.util.List;
@@ -24,6 +27,71 @@ public class AppTemplate extends BaseBBTemplate {
             sb.append(">").append(toStr.apply(item)).append("</option>");
         }
         return sb.toString();
+    }
+
+    public String getBreadcrumb(Application selectedApp, Version selectedVersion, Release selectedRelease, String path) {
+        StringBuilder sb = new StringBuilder("<ol class=\"breadcrumb\">\n" +
+                "    <li><a href=\"/\">Home</a></li>\n");
+        sb.append("    <li><a href=\"").append(pathFor(selectedApp)).append("\" class=\"breadcrumb-item\">").append(selectedApp.getName()).append("</a></li>\n");
+        sb.append("    <li><a href=\"").append(pathFor(selectedApp, selectedVersion)).append("\" class=\"breadcrumb-item\">").append(selectedVersion.getName()).append("</a></li>\n");
+        sb.append("    <li><a href=\"").append(pathFor(selectedApp, selectedVersion, selectedRelease))
+                .append("\" class=\"breadcrumb-item");
+        if (path.length() == 0) {
+            sb.append(" active");
+        }
+        sb.append("\">").append(selectedRelease.getName()).append("</a></li>\n");
+
+        if (path.length() > 0) {
+            String[] pathArray = path.split("/");
+            int i = 0;
+            boolean zipped = false;
+            StringBuilder pathBuilder = new StringBuilder();
+            while (i < pathArray.length) {
+                String currentResource = pathArray[i];
+                if (zipped) {
+                    i += 1;
+                    while (i < pathArray.length && !(currentResource.endsWith(".zip") || currentResource.endsWith(".jar"))) {
+                        currentResource = currentResource + "/" + pathArray[i++];
+                    }
+                    i -= 1;
+                }
+                if (currentResource.endsWith(".zip") || currentResource.endsWith(".jar")) {
+                    zipped = true;
+                }
+                pathBuilder.append(currentResource).append("/");
+                i += 1;
+                sb.append("    <li><a href=\"").append(pathFor(selectedApp, selectedVersion, selectedRelease)).append(pathBuilder).append("\" class=\"breadcrumb-item");
+                if (i == pathArray.length) {
+                    sb.append(" active");
+                }
+                sb.append("\">").append(currentResource).append("</a></li>\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String pathFor(Application selectedApp) {
+        return "/" + selectedApp.getFileSystemName();
+    }
+
+    private String pathFor(Application selectedApp, Version selectedVersion) {
+        return pathFor(selectedApp) + "/" + selectedVersion.getName();
+    }
+
+    private String pathFor(Application selectedApp, Version selectedVersion, Release selectedRelease) {
+        return pathFor(selectedApp, selectedVersion) + "/" + selectedRelease.getName() + "/";
+    }
+
+    public String pathFor(Application selectedApp, Version selectedVersion, Release selectedRelease, String path) {
+            return pathFor(selectedApp, selectedVersion, selectedRelease) + path + "/" ;
+    }
+
+    public String pathFor(Application selectedApp, Version selectedVersion, Release selectedRelease, Resource selectedResource, String path) {
+        if (path != null && path.length() > 0) {
+            return pathFor(selectedApp, selectedVersion, selectedRelease, path) + selectedResource.getName() + "/";
+        } else {
+            return pathFor(selectedApp, selectedVersion, selectedRelease) + selectedResource.getName() + "/";
+        }
     }
 
 
